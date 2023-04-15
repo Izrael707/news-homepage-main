3a1049508a5075fb61e95992266b368c72683b81
izrael707 <dkingisreal@gmail.com>
2023-04-15 21:44:55 +0100

    component1 done

---
+++ components/Component1.jsx
@@ -0,0 +1,20 @@
+export default function Component1() {
+  return (
+    <div className="mt-4">
+      <img src="/images/image-web-3-mobile.jpg" alt="web-3-mobile" />
+      <div className="my-2">
+        <p className="font-bold text-5xl my-2">The Bright Future of Web 3.0?</p>
+        <div>
+          <p className="text-neutral-600 text-lg font-medium my-2">
+            We dive into the next evolution of the web that claims to put the
+            power of the platforms back into the hands of the people. But is it
+            really fulfilling its promise?
+          </p>
+          <button className="px-12 py-4 text-lg my-2 bg-red-500 uppercase font-medium text-white tracking-widest">
+            Read more
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import Component1 from "../components/Component1";
+import Navbar from "../components/Navbar";
+
+export default function App() {
+  return (
+    <div className="h-screen p-4">
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+
+        <title>Frontend Mentor | News homepage</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Component1 />
+        {/* <div class="">
+          Challenge by
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Izrael707</a>.
+        </div> */}
+      </main>
+    </div>
+  );
+}
